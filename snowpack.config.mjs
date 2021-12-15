84139ff37226f1800388f29d16a057dd51132ed8
GamingInfinite <52724070+GamingInfinite@users.noreply.github.com>
2021-12-15 14:57:43 -0600

    build to the docs folder

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -25,6 +25,9 @@ export default {
     /* ... */
   },
   buildOptions: {
-    /* ... */
+    // put the build files in /docs
+    out: 'docs',
+    // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
+    metaUrlPath: 'snowpack'
   },
 };
